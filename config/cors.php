4c15514a1a6dc1c4665b4086f46a32361e95db4e
Emma Karanja <karanjaemmak@gmail.com>
2026-02-25 12:46:02 +0300

    Fix CORS configuration for frontend
    - Remove trailing slash from origin URL

--- config/cors.php
+++ config/cors.php
@@ -20,6 +20,7 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
+        'https://symatech-assesment-frontend.vercel.app',
         'https://symatech-assesment-frontend.vercel.app/',
     ],
 
